e491b95533e5aa8043f4925e046ba6e974795ea8
Miguel Cruz <miguel.cruz@sistemashumanos.com>
2020-02-14 15:21:31 -0600

    initial_commit

---
+++ lib/Model/CatalogoMoneda.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace RccClientSimulacion\Client\Model;
+use \RccClientSimulacion\Client\ObjectSerializer;
+
+class CatalogoMoneda
+{
+    
+    const MX = 'MX';
+    const US = 'US';
+    const UD = 'UD';
+    
+    
+    public static function getAllowableEnumValues()
+    {
+        return [
+            self::MX,
+            self::US,
+            self::UD,
+        ];
+    }
+}
