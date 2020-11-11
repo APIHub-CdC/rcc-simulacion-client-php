49c781b40d3402bdb173dbb654894c5b83d009ae
BryantCancino <bryantcancino@gmail.com>
2020-11-11 16:54:28 -0600

    se actualiza namespace

---
+++ lib/Model/CatalogoMoneda.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace RCC\Simulacion\MX\Client\Model;
+use \RCC\Simulacion\MX\Client\ObjectSerializer;
+
+class CatalogoMoneda
+{
+    
+    const MX = 'MX';
+    const US = 'US';
+    const UD = 'UD';
+    
+    
+    public static function getAllowableEnumValues()
+    {
+        return [
+            self::MX,
+            self::US,
+            self::UD,
+        ];
+    }
+}
---
+++ lib/Model/CatalogoSexo.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace RCC\Simulacion\MX\Client\Model;
+use \RCC\Simulacion\MX\Client\ObjectSerializer;
+
+class CatalogoSexo
+{
+    
+    const F = 'F';
+    const M = 'M';
+    
+    
+    public static function getAllowableEnumValues()
+    {
+        return [
+            self::F,
+            self::M,
+        ];
+    }
+}
